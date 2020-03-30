0ff4bd7e4cffb52574398709a3902d42ba8291d4
Paul Barthmaier <paul@barthmaier.rocks>
2020-03-30 16:51:02 -0400

    datetime story try

---
+++ packages/components/src/date-time/stories/index.js
@@ -0,0 +1,11 @@
+
+/**
+ * Internal dependencies
+ */
+import DateTime from '../';
+
+export default { title: 'Components/DateTime', component: DateTime };
+
+export const _default = () => {
+	return <DateTime />;
+};
